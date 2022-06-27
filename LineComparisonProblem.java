4a89feaa45136005f70a28039de1a4a3ff522c40
BhaktiRohane2404 <98884739+BhaktiRohane2404@users.noreply.github.com>
2022-06-27 09:00:32 +0530

    Add files via upload

---
+++ LineComparisonProblem.java
@@ -0,0 +1,13 @@
+package com.linecComparision.Oops;
+
+public class LineComparisonProblem {
+
+    public void welcomeOnMasterBranch() {
+        System.out.println("Welcome to the employee wage computation program.");
+    }
+
+    public static void main(String[] args) {
+        LineComparisonProblem lineComparisonProblem =new LineComparisonProblem();
+        lineComparisonProblem.welcomeOnMasterBranch();
+    }
+}
